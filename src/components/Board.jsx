30dd27b0e1a9664a22d1534005e138a464dcbfa0
munimrahman <munimrh@gmail.com>
2022-06-08 01:25:05 +0600

    game done

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import Square from './Square';
 
 class Board extends React.Component {
+    renderSquare = i => <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+    />
     render() {
+        const { } = this.props;
         return (
             <div>
-
+                <div>
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div>
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div>
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
             </div>
         );
     }
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 class Square extends React.Component {
     render() {
+        const { value, onClick } = this.props;
         return (
-            <div>
-
-            </div>
+            <button className='border border-gray-500 py-6 px-7' onClick={onClick}>
+                {value}
+            </button>
         );
     }
 }
