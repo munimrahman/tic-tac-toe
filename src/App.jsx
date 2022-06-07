a77f1299ca9374a9320bd2d881d0ff83648f2a20
munimrahman <munimrh@gmail.com>
2022-06-07 20:30:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Game from './components/Game';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Game />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Game.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 className='text-3xl'>Tic Tac Toe</h1>
+            </div>
+        );
+    }
+}
+
+export default Game;
